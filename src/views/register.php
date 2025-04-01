aed90d850f1b7f9d0dedc8fd93d2c65d491b348a
cjaylao <cjaylao0327@gmail.com>
2025-04-01 18:12:05 +0800

    added forms in login and register

---
+++ src/views/register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+<div style="justify-content:center; display:flex;">
+    <h1>Register</h1> 
+    <form action="POST">
+        <label for="Email">Email</label><br>
+        <input type="text"><br>
+        <label for="Password">Password</label><br>
+        <input type="Password"><br>
+        <label for="Password">Re-type Password</label> <br>
+        <input type="Password"><br>
+        <button>Submit</button>
+    </form>
+</div>
+</body>
+</html>
